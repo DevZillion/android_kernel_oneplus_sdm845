5d0c85c849ac329e4c3d4cfad17c7827e5bccd19
DevZillion <devzillion@proton.me>
2023-10-28 10:37:31 +0300

    push build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clangpath=$(pwd)/toolchains/clang-r416183b
+gccpath=$(pwd)/toolchains/aarch64-linux-android-4.9
+path=$(pwd)
+
+cd $path
+git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b -b lineage-20.0 --depth=1 toolchains/clang-r416183b
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android12-release --depth=1 toolchains/aarch64-linux-android-4.9
+clear
+
+make O=out ARCH=arm64 mcd_defconfig
+PATH=$clangpath/bin:$gccpath/bin:${PATH} \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android-
